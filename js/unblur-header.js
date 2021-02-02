e96458e9c177f97d3c3dbe6e2c6388cde0b89ce0
Michał <mwargan@gmail.com>
2021-02-02 11:24:17 +0100

    Fixes too wide tables and un-blur touch event

--- js/unblur-header.js
+++ js/unblur-header.js
@@ -5,32 +5,48 @@
  * navigation support for dropdown menus.
  */
 ( function() {
-	var element = document.getElementById("header_image");
+	let timer;
+	let element = document.getElementById("header_image");
 
-	var exposeBackground = function (event) {
-
-		// Prevents iOS from highlighting stuff when long pressing
-		event.returnValue = false;
+	var exposeBackground = function () {
 
 		element.classList.remove("bg-blur");
 		element.classList.remove("has-background-dim");
+
 		element.children[0].style.opacity = 0;
 		element.children[1].style.display = "none";
 	};
 
-	var obscureBackground = function (event) {
-
-		// Prevents iOS from highlighting stuff when long pressing
-		event.returnValue = false;
+	var obscureBackground = function () {
 
 		element.classList.add("bg-blur");
 		element.classList.add("has-background-dim");
 		element.children[0].style.opacity = null;
 	};
 
-	element.addEventListener('touchstart', exposeBackground, false);
+	  element.addEventListener('touchstart', (event) => {
+	    timer = setTimeout(() => {
+	      timer = null;
+	      // Prevents iOS from highlighting stuff when long pressing
+	  	  document.body.style['-webkit-user-select'] = 'none';
+
+	      exposeBackground();
+	    }, 300);
+	  });
+
+	  function cancel(event) {
+		document.body.style['-webkit-user-select'] = null;
+	    clearTimeout(timer);
+	    obscureBackground(element);
+	  }
+
+	  element.addEventListener('touchend', cancel);
+	  element.addEventListener('touchmove', cancel);
+
+
+	// element.addEventListener('touchstart', exposeBackground, false);
 	element.addEventListener('mousedown', exposeBackground, false);
 
-	element.addEventListener('touchend', obscureBackground, false);
+	// element.addEventListener('touchend', obscureBackground, false);
 	element.addEventListener('mouseup', obscureBackground, false);
 } )();
